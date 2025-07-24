a67c76241be496531b06f244a7d75878efb39f1f
JC Li <zeyo0510@gmail.com>
2025-07-25 00:48:21 +0800

    Create JC.Lib.CS

---
+++ JC.Lib.CS/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+/************************************************/
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCulture("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyProduct("JC")]
+[assembly: AssemblyTitle("JC.Lib.CS")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyVersion("2025.07.25")]
+[assembly: ComVisible(false)]
