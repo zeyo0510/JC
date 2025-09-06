6e7d907857c3d340f769f20faf23b5cc0b11a2d9
JC Li <zeyo0510@gmail.com>
2025-09-06 16:07:31 +0800

    Create GraphicsExtensions[DrawPixel]

---
+++ JC.Lib.CS/Common/GraphicsExtensions.DrawPixel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+/************************************************/
+namespace JC.Lib.CS.Common
+{
+  static partial class GraphicsExtensions
+  {
+    public static void DrawPixel(this Graphics g, int x, int y, Color color)
+    {
+      using (SolidBrush brush = new SolidBrush(color))
+      {
+        g.FillRectangle(brush, x, y, 1, 1);
+      }
+    }
+  }
+}
